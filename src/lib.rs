49104be33defcd1d62a1ce7a2647c12929b79649
me-it-is <188520719+me-it-is@users.noreply.github.com>
2026-03-11 17:48:25 -0700

    add basics of a swerve module

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+mod swerve_module;
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
---
+++ src/swerve_module/mod.rs
@@ -0,0 +1,18 @@
+use rapier3d::prelude::RigidBodyHandle;
+use whippyunits::unit;
+
+struct SwerveModule {
+    pub config: SwerveModuleConfig,
+    pub wheel_handle: RigidBodyHandle,
+    pub azumith_handle: RigidBodyHandle,
+}
+
+#[derive(Clone, Copy, Debug)]
+struct SwerveModuleConfig {
+    pub wheel_radius: unit!(m, f32),
+    pub wheel_width: unit!(m, f32),
+    pub drive_moi: unit!(kg * m ^ 2, f32),
+    pub azumith_moi: unit!(kg * m ^ 2, f32),
+    pub wheel_center_height: unit!(m, f32),
+    pub wheel_cof: f32,
+}
