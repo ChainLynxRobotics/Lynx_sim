f4db7c46b64ae3aeb821ea9784d6b192f49e9583
me-it-is <188520719+me-it-is@users.noreply.github.com>
2026-03-28 14:36:12 -0700

    create a new function for swerve modules

--- src/swerve_module/mod.rs
+++ src/swerve_module/mod.rs
@@ -1,11 +1,99 @@
-use rapier3d::prelude::RigidBodyHandle;
-use whippyunits::unit;
+use std::f32::consts::PI;
+
+use rapier3d::{
+    math::{Vec3, Vector},
+    prelude::{
+        ColliderBuilder, ColliderSet, MassProperties, MultibodyJointSet, RevoluteJointBuilder,
+        RigidBodyBuilder, RigidBodyHandle, RigidBodySet,
+    },
+};
+use whippyunits::{unit, value};
 
 #[derive(Debug, PartialEq)]
 struct SwerveModule {
     pub config: SwerveModuleConfig,
     pub wheel_handle: RigidBodyHandle,
     pub azumith_handle: RigidBodyHandle,
+}
+impl SwerveModule {
+    pub fn new(
+        config: SwerveModuleConfig,
+        module_top_center: Vector,
+        drive_base_handle: RigidBodyHandle,
+        rigid_body_set: &mut RigidBodySet,
+        collider_set: &mut ColliderSet,
+        joint_set: &mut MultibodyJointSet,
+    ) -> Self {
+        let wheel = RigidBodyBuilder::dynamic()
+            .translation(
+                module_top_center - Vec3::new(0.0, 0.0, value!(config.wheel_center_height, m, f32)),
+            )
+            .build();
+        let wheel_colider = ColliderBuilder::cylinder(
+            value!(config.wheel_width, m, f32) / 2.0,
+            value!(config.wheel_radius, m, f32),
+        )
+        .friction(config.wheel_cof)
+        .restitution(config.wheel_coefficient_of_restetution)
+        .mass_properties(MassProperties::new(
+            Vec3::ZERO,
+            value!(config.wheel_mass, kg, f32),
+            Vec3::new(
+                value!(config.wheel_secondary_moi, kg * m ^ 2, f32),
+                value!(config.drive_moi, kg * m ^ 2, f32),
+                value!(config.wheel_secondary_moi, kg * m ^ 2, f32),
+            ),
+        ))
+        .build();
+        let azumith = RigidBodyBuilder::dynamic()
+            .translation(
+                module_top_center
+                    - Vec3::new(0.0, 0.0, value!(config.azumith_center_height, m, f32)),
+            )
+            .build();
+        let azumith_colider = ColliderBuilder::cylinder(
+            value!(config.azumith_thickness, m, f32) / 2.0,
+            value!(config.azumith_radius, m, f32),
+        )
+        .mass_properties(MassProperties::new(
+            Vec3::ZERO,
+            value!(config.azumith_mass, kg, f32),
+            Vec3::new(
+                value!(config.azumith_secondary_moi, kg * m ^ 2, f32),
+                value!(config.azumith_moi, kg * m ^ 2, f32),
+                value!(config.azumith_secondary_moi, kg * m ^ 2, f32),
+            ),
+        ))
+        .rotation(Vec3::new(PI, 0.0, 0.0));
+
+        let wheel = rigid_body_set.insert(wheel);
+        let azumith = rigid_body_set.insert(azumith);
+
+        collider_set.insert_with_parent(wheel_colider, wheel, rigid_body_set);
+        collider_set.insert_with_parent(azumith_colider, azumith, rigid_body_set);
+
+        let wheel_joint = RevoluteJointBuilder::new(Vec3::Y)
+            .local_anchor1(Vec3::ZERO)
+            .local_anchor2(
+                module_top_center - Vec3::new(0.0, 0.0, value!(config.wheel_center_height, m, f32)),
+            )
+            .build();
+        let azumith_joint = RevoluteJointBuilder::new(Vec3::Z)
+            .local_anchor1(Vec3::ZERO)
+            .local_anchor2(
+                module_top_center
+                    - Vec3::new(0.0, 0.0, value!(config.azumith_center_height, m, f32)),
+            )
+            .build();
+
+        joint_set.insert(wheel, drive_base_handle, wheel_joint, true);
+        joint_set.insert(azumith, drive_base_handle, azumith_joint, true);
+        return SwerveModule {
+            config,
+            wheel_handle: wheel,
+            azumith_handle: azumith,
+        };
+    }
 }
 
 #[derive(Clone, Copy, Debug, PartialEq)]
