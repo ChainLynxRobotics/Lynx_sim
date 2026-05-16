bd3d34c12e0cff9ff5aef519287771e92d47ca0d
me-it-is <188520719+me-it-is@users.noreply.github.com>
2026-05-16 11:19:34 -0700

    move, movement code out of debug_render

--- testing-app/src/main.rs
+++ testing-app/src/main.rs
@@ -5,6 +5,7 @@ use rapier3d::{
     math::{Pose3, Vec3},
     na::Translation3,
 };
+use sdl3::{event::Event, keyboard::Keycode};
 use std::time::Duration;
 use swerve_sim_3d::{
     physics_world,
@@ -54,6 +55,148 @@ fn main() {
             &physics_world.multibody_joint_set,
             &physics_world.narrow_phase,
         );
+        for event in window.event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => panic!("uhh close the app i guess"),
+                Event::KeyDown {
+                    keycode: Some(Keycode::W),
+                    ..
+                } => {
+                    window.camera.pose = window.camera.pose.append_translation(Vec3 {
+                        x: 0.01,
+                        y: 0.0,
+                        z: 0.0,
+                    })
+                }
+                Event::KeyDown {
+                    keycode: Some(Keycode::S),
+                    ..
+                } => {
+                    window.camera.pose = window.camera.pose.append_translation(Vec3 {
+                        x: -0.01,
+                        y: 0.0,
+                        z: 0.0,
+                    })
+                }
+                Event::KeyDown {
+                    keycode: Some(Keycode::A),
+                    ..
+                } => {
+                    window.camera.pose = window.camera.pose.append_translation(Vec3 {
+                        x: 0.0,
+                        y: 0.01,
+                        z: 0.0,
+                    })
+                }
+                Event::KeyDown {
+                    keycode: Some(Keycode::D),
+                    ..
+                } => {
+                    window.camera.pose = window.camera.pose.append_translation(Vec3 {
+                        x: 0.0,
+                        y: -0.01,
+                        z: 0.0,
+                    })
+                }
+                Event::KeyDown {
+                    keycode: Some(Keycode::Q),
+                    ..
+                } => {
+                    window.camera.pose = window.camera.pose.append_translation(Vec3 {
+                        x: 0.0,
+                        y: 0.0,
+                        z: -0.01,
+                    })
+                }
+                Event::KeyDown {
+                    keycode: Some(Keycode::E),
+                    ..
+                } => {
+                    window.camera.pose = window.camera.pose.append_translation(Vec3 {
+                        x: 0.0,
+                        y: 0.0,
+                        z: 0.01,
+                    })
+                }
+                Event::KeyDown {
+                    keycode: Some(Keycode::Left),
+                    ..
+                } => {
+                    let current_rot = window
+                        .camera
+                        .pose
+                        .rotation
+                        .to_euler(rapier3d::glamx::EulerRot::XYZ);
+                    window.camera.pose = Pose3::new(
+                        window.camera.pose.translation,
+                        Vec3 {
+                            x: current_rot.0,
+                            y: current_rot.1,
+                            z: current_rot.2 + 0.05,
+                        },
+                    )
+                }
+                Event::KeyDown {
+                    keycode: Some(Keycode::Right),
+                    ..
+                } => {
+                    let current_rot = window
+                        .camera
+                        .pose
+                        .rotation
+                        .to_euler(rapier3d::glamx::EulerRot::XYZ);
+                    window.camera.pose = Pose3::new(
+                        window.camera.pose.translation,
+                        Vec3 {
+                            x: current_rot.0,
+                            y: current_rot.1,
+                            z: current_rot.2 - 0.05,
+                        },
+                    )
+                }
+                Event::KeyDown {
+                    keycode: Some(Keycode::Up),
+                    ..
+                } => {
+                    let current_rot = window
+                        .camera
+                        .pose
+                        .rotation
+                        .to_euler(rapier3d::glamx::EulerRot::XYZ);
+                    window.camera.pose = Pose3::new(
+                        window.camera.pose.translation,
+                        Vec3 {
+                            x: current_rot.0,
+                            y: current_rot.1 + 0.05,
+                            z: current_rot.2,
+                        },
+                    )
+                }
+                Event::KeyDown {
+                    keycode: Some(Keycode::Down),
+                    ..
+                } => {
+                    let current_rot = window
+                        .camera
+                        .pose
+                        .rotation
+                        .to_euler(rapier3d::glamx::EulerRot::XYZ);
+                    window.camera.pose = Pose3::new(
+                        window.camera.pose.translation,
+                        Vec3 {
+                            x: current_rot.0,
+                            y: current_rot.1 - 0.05,
+                            z: current_rot.2,
+                        },
+                    )
+                }
+                _ => {}
+            }
+        }
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 }
