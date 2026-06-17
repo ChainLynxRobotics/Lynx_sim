f61786cba732f643fb6236b612b868f55ad59f38
me-it-is <188520719+me-it-is@users.noreply.github.com>
2026-06-17 13:11:29 -0700

    update types

--- ipc-types/src/lib.rs
+++ ipc-types/src/lib.rs
@@ -1,17 +1,17 @@
 use ipc_channel::ipc::IpcSender;
 
-#[derive(Debug, Clone, Copy, serde::Deserialize, serde::Serialize)]
+#[derive(Debug, Clone, Copy, serde::Deserialize, serde::Serialize, PartialEq)]
 pub struct DebugLine {
     pub point1: (f32, f32, f32),
     pub point2: (f32, f32, f32),
     pub color: (f32, f32, f32, f32),
 }
-#[derive(Debug, Clone, Copy, serde::Deserialize, serde::Serialize)]
+#[derive(Debug, Clone, Copy, serde::Deserialize, serde::Serialize, PartialEq)]
 pub enum Message {
     Line(DebugLine),
     StartTransfer,
     EndTransfer,
 }
 pub struct DebugWindow {
-    sender: IpcSender<DebugLine>,
+    pub sender: IpcSender<Message>,
 }
