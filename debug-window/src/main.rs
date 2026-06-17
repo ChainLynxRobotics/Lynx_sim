5a33ddd2f7a24c9b6b25b5ecf652233a6da7f46f
me-it-is <188520719+me-it-is@users.noreply.github.com>
2026-06-17 14:28:31 -0700

    move app to its own file

--- debug-window/src/main.rs
+++ debug-window/src/main.rs
@@ -1,117 +1,19 @@
 use std::env;
 use std::result::Result::Ok;
-use std::sync::Arc;
-use std::time::{Duration, Instant};
 
 use ipc_channel::ipc;
 use ipc_channel::ipc::{IpcReceiver, IpcSender};
 use ipc_types::Message;
-use winit::event::StartCause;
 use winit::event_loop::ControlFlow;
-use winit::{
-    application::ApplicationHandler,
-    event::{KeyEvent, WindowEvent},
-    event_loop::{ActiveEventLoop, EventLoop},
-    keyboard::PhysicalKey,
-    window::Window,
-};
+use winit::event_loop::EventLoop;
 
-use crate::state::State;
+use crate::app::App;
 
+mod app;
 mod camera;
 mod state;
 mod vertex;
 
-pub struct App {
-    state: Option<State>,
-    render_target: Instant,
-    line_receiver: Arc<IpcReceiver<Message>>,
-}
-impl App {
-    pub fn new(line_receiver: IpcReceiver<Message>) -> Self {
-        Self {
-            state: None,
-            render_target: Instant::now(),
-            line_receiver: Arc::new(line_receiver),
-        }
-    }
-}
-impl ApplicationHandler<State> for App {
-    fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
-        let mut window_attributes = Window::default_attributes();
-
-        let window = Arc::new(event_loop.create_window(window_attributes).unwrap());
-
-        self.state =
-            Some(pollster::block_on(State::new(window, self.line_receiver.clone())).unwrap());
-    }
-
-    fn user_event(&mut self, event_loop: &winit::event_loop::ActiveEventLoop, event: State) {
-        self.state = Some(event);
-    }
-
-    fn window_event(
-        &mut self,
-        event_loop: &winit::event_loop::ActiveEventLoop,
-        window_id: winit::window::WindowId,
-        event: winit::event::WindowEvent,
-    ) {
-        let state = match &mut self.state {
-            Some(canvas) => canvas,
-            None => return,
-        };
-
-        match event {
-            WindowEvent::CloseRequested => event_loop.exit(),
-            WindowEvent::Resized(size) => state.resize(size.width, size.height),
-            WindowEvent::RedrawRequested => {
-                state.update();
-                match state.render() {
-                    Ok(_) => {}
-                    Err(e) => {
-                        log::error!("{e}");
-                        event_loop.exit();
-                    }
-                }
-
-                let now = Instant::now();
-                if self.render_target <= now {
-                    self.render_target = now + Duration::from_secs_f32(FRAME_TIME);
-                    state.window.request_redraw();
-                }
-            }
-            WindowEvent::KeyboardInput {
-                event:
-                    KeyEvent {
-                        physical_key: PhysicalKey::Code(code),
-                        state: key_state,
-                        ..
-                    },
-                ..
-            } => state.handle_key(event_loop, code, key_state.is_pressed()),
-            _ => {}
-        }
-    }
-
-    fn new_events(&mut self, _: &ActiveEventLoop, _: StartCause) {
-        if self.render_target <= Instant::now() {
-            self.render_target += Duration::from_secs_f32(FRAME_TIME);
-            let state = match &mut self.state {
-                Some(canvas) => canvas,
-                None => return,
-            };
-            state.window.request_redraw();
-        };
-    }
-
-    fn about_to_wait(&mut self, event_loop: &ActiveEventLoop) {
-        // make sure we don't sleep past next render target time
-        event_loop.set_control_flow(ControlFlow::WaitUntil(self.render_target));
-    }
-}
-
-const FPS: f32 = 30.0;
-const FRAME_TIME: f32 = 1.0 / FPS;
 pub fn run(line_receiver: IpcReceiver<Message>) -> anyhow::Result<()> {
     env_logger::init();
 
