53f124da7b193cbd3709a9cdb5970e91845b03c1
me-it-is <188520719+me-it-is@users.noreply.github.com>
2026-07-07 13:16:38 -0700

    add const definitions for interaction groups

--- swerve-sim-lib/src/lib.rs
+++ swerve-sim-lib/src/lib.rs
@@ -1,3 +1,17 @@
+use rapier3d::geometry::{Group, InteractionGroups, InteractionTestMode::Or};
+
 pub mod physics_world;
 pub mod swerve_module;
 pub mod util;
+
+pub const ROBOT_INTERACTION_GROUPS: InteractionGroups = InteractionGroups {
+    memberships: Group::GROUP_2,
+    filter: Group::GROUP_1,
+    test_mode: Or,
+};
+
+pub const FIELD_INTERACTION_GROUPS: InteractionGroups = InteractionGroups::new(
+    Group::GROUP_1,
+    Group::union(Group::GROUP_1, Group::GROUP_2),
+    Or,
+);
