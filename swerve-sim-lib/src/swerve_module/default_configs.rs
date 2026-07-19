6f2d45a5b58d1bd7b91505697de1fecc92b276be
me-it-is <188520719+me-it-is@users.noreply.github.com>
2026-07-19 16:18:56 -0700

    Remove zero config

--- swerve-sim-lib/src/swerve_module/default_configs.rs
+++ swerve-sim-lib/src/swerve_module/default_configs.rs
@@ -4,26 +4,6 @@ use whippyunits::{quantity, unit};
 
 #[culit(whippyunits::default_declarators::literals)]
 impl SwerveModuleConfig {
-    pub const ZERO: Self = Self::new(
-        0.0m_f32,
-        0.0m_f32,
-        0.0kg_f32,
-        quantity!(0.0, kg * m ^ 2, f32),
-        quantity!(0.0, kg * m ^ 2, f32),
-        quantity!(0.0, kg * m ^ 2, f32),
-        quantity!(0.0, kg * m ^ 2, f32),
-        0.0kg_f32,
-        0.0m_f32,
-        0.0m_f32,
-        0.0m_f32,
-        0.0m_f32,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-    );
-
     pub const fn get_mk4i(_gear_ratio: Mk4iGearRatio, _wheel: Mk4iWheel) -> Self {
         todo!()
     }
